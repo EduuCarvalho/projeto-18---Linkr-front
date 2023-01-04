4589ef85ae21799af9991f23a1bab3c2e80abef0
EduuCarvalho <educarvalho.engcivil@gmail.com>
2023-01-04 20:57:15 -0300

    debug: comentei import USER PROVIDER page app.js

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GlobalStyle from "../global/globalStyle";
-import { UserProvider } from "../global/userContext";
+/* import { UserProvider } from "../global/userContext"; */
 /* import Home from "./home/home"; // A pagina "/" é a rota signIn */
 import SignIn from "./Login/Sign-in.js"
 import SignUp from "./Login/sign-up.js"
