a0faa4b54165aa47344c65dcb63f1902ee6b169b
EduuCarvalho <educarvalho.engcivil@gmail.com>
2023-01-04 20:39:10 -0300

    feat: conflito de repositorio ap..js login resolvi

--- src/pages/app.js
+++ src/pages/app.js
@@ -2,8 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GlobalStyle from "../global/globalStyle";
 import { UserProvider } from "../global/userContext";
 /* import Home from "./home/home"; // A pagina "/" é a rota signIn */
-import SignIn from "../pages/Login/Sign-in.js"
-import SignUp from "../pages/Login/sign-up.js";
+import SignIn from "./Login/Sign-in.js"
+import SignUp from "./Login/sign-up.js"
 import UserInfoProvider from "../contexts/userInfo";
 import Home from "./home/home";
 
